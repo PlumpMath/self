d1f6abb85a28cb9dcee579b71d4034fef4b885e6
Lucas Stadler <mi.au@papill0n.org>
2013-07-23 22:44:52 +0200

    Display a comment on hover.

--- niko-niko.js
+++ niko-niko.js
@@ -1,4 +1,7 @@
-var dailyHappiness = range(0, 100).map(function(_) { return Math.floor(Math.random() * 9); });
+var dailyHappiness = range(0, 100).map(function(_) {
+	return {mood: Math.floor(Math.random() * 9),
+	        comment: String.fromCharCode(Math.random() * 1000)};
+});
 
 var stage = d3.select("#happiness");
 var cellSize = 20;
@@ -7,6 +10,10 @@ var colorScale = d3.scale.ordinal()
 	.domain(range(0, 9))
 	.range(colorbrewer.YlGn[9]);
 
+stage.on("mouseover", function() {
+	d3.select("#description").text(d3.event.target.__data__.comment);
+});
+
 var makeDaySquare = function(sel) {
 	sel
 	.attr("width", cellSize + "px")
@@ -14,7 +21,7 @@ var makeDaySquare = function(sel) {
 	.attr("x", function(_, i) { return i % 7 * cellSize; })
 	.attr("y", function(_, i) { return Math.floor(i / 7) * cellSize; })
 	.attr("stroke", "#000").attr("stroke-width", "0.1px")
-	.attr("fill", function(d) { return colorScale(d); });
+	.attr("fill", function(d) { return colorScale(d.mood); });
 }
 
 stage.selectAll("rect").data(dailyHappiness)
