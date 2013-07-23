a3a42916a51d31a594e1d459d0c9645cb4067ba7
Lucas Stadler <mi.au@papill0n.org>
2013-07-23 22:04:47 +0200

    Put the xhr'd squares under the correct node.

--- niko-real.js
+++ niko-real.js
@@ -3,8 +3,8 @@ var activityUrl = "./sample.json";
 var stage = d3.select("#happiness");
 
 d3.json(activityUrl, function(activity) {
-	d3.selectAll("rect").remove();
-	d3.selectAll("rect").data(activity.map(function(a){return a.mood}))
+	stage.selectAll("rect").remove();
+	stage.selectAll("rect").data(activity.map(function(a){return a.mood}))
 		.enter().append("rect")
 		.call(makeDaySquare);
 });
