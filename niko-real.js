8d5959d9028e5ac219d52b18c4f8e01bef1c30c9
Lucas Stadler <mi.au@papill0n.org>
2013-07-23 22:46:37 +0200

    Display comment when using the external datasource.

--- niko-real.js
+++ niko-real.js
@@ -4,7 +4,7 @@ var stage = d3.select("#happiness");
 
 d3.json(activityUrl, function(activity) {
 	stage.selectAll("rect").remove();
-	stage.selectAll("rect").data(activity.map(function(a){return a.mood}))
+	stage.selectAll("rect").data(activity)
 		.enter().append("rect")
 		.call(makeDaySquare);
 });
