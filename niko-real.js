402bb0c729ac26cf03595f4991ba0fba6afb4b9b
Lucas Stadler <mi.au@papill0n.org>
2013-08-02 00:10:32 +0200

    Set correct startDay with remote happiness data.

--- niko-real.js
+++ niko-real.js
@@ -3,6 +3,7 @@ var activityUrl = "http://papill0n.org/happiness.json";
 var stage = d3.select("#happiness");
 
 d3.json(activityUrl, function(activity) {
+	startDay = toDays(new Date(activity[0].date));
 	stage.selectAll("rect").remove();
 	stage.selectAll("rect").data(activity)
 		.enter().append("rect")
